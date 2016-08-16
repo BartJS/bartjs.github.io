aec68fcd2c8b1df647de84ef5a5428852cd0ed24
Mikael Brevik <mikaelbre@gmail.com>
2016-08-16 13:36:43 +0200

    Changes to codeinthedark site

--- browser/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    API_KEY: '2e860262b1354144322675c5e3f463d'
-};
--- browser/parser.js
+++
@@ -1,6 +0,0 @@
-module.exports.members = function (data) {
-  if (!data.results) return [];
-  return data.results;
-};
-
-module.exports.events = module.exports.members;
